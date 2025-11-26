a7779e152c1363345da720255cb7ac6baad7fdf8
iberiaforestal <iberiaforestal.ing@gmail.com>
2025-11-26 12:31:20 +0100

    Update madrid.py

--- madrid.py
+++ madrid.py
@@ -1613,110 +1613,21 @@ def generar_pdf(datos, x, y, filename):
         # Volver a negrita para el resto del texto
     pdf.set_font("Arial", "B", 9)  # Restaurar negrita
     texto_final = (
-        "\nDe acuerdo con lo establecido en el artículo 22 de la ley 43/2003 de 21 de noviembre de Montes, toda inmatriculación o inscripción de exceso de cabida en el Registro de la Propiedad de un monte o de una finca colindante con monte demanial o ubicado en un término municipal en el que existan montes demaniales requerirá el previo informe favorable de los titulares de dichos montes y, para los montes catalogados, el del órgano forestal de la comunidad autónoma.\n\n"
-        "En cuanto a vías pecuarias, salvaguardando lo que pudiera resultar de los futuros deslindes, en las parcelas objeto este informe-borrador, cualquier construcción, plantación, vallado, obras, instalaciones, etc., no deberían realizarse dentro del área delimitada como dominio público pecuario provisional para evitar invadir éste.\n\n"
+        "\nLas afecciones del presente informe se basan en cartografia oficial de la Comunidad Autonoma y de la Direccion General del Catastro, cumpliendo el estandar tecnico Web Feature Service (WFS) definido por el Open Geospatial Consortium (OGC) y la Directiva INSPIRE, eximiendo a IBERIA FORESTAL INGENIERIA S.L de cualquier error en la cartografia.\n\n"
+        "El Planeamiento se regira por la Ley 9/2001, de 17 de julio, del Suelo, de la Comunidad de Madrid, y por el PGOU del termino municipal. El Regimen del suelo no urbanizable de protección se recoge en el articulo 29 de la citada Ley.\n\n"
+        "En suelo no urbanizable se prestara especial atencion al artículo 3 de la Ley 16/1995, de 4 de mayo, Forestal y de Protección de la Naturaleza de la Comunidad de Madrid, y al artículo 5 de la Ley 43/2003, de 21 de noviembre, de Montes. Solicitando para posibles cambios de uso lo establecido en la normativa de referencia.\n\n"
+        "De acuerdo con lo establecido en el articulo 22.1 de la ley 43/2003 de 21 de noviembre de Montes, toda inmatriculacion o inscripcion de exceso de cabida en el Registro de la Propiedad de un monte o de una finca colindante con monte demanial o ubicado en un termino municipal en el que existan montes demaniales requerira el previo informe favorable de los titulares de dichos montes y, para los montes catalogados, el del organo forestal de la comunidad autonoma.\n\n"
+        "De acuerdo con lo establecido en el articulo 25.5 de la ley 43/2003 de 21 de noviembre de Montes, para posibilitar el ejercicio del derecho de adquisicion preferente a traves de la accion de tanteo, el transmitente debera notificar fehacientemente a la Administracion publica titular de ese derecho los datos relativos al precio y caracteristicas de la transmision proyectada, la cual dispondra de un plazo de tres meses, a partir de dicha notificacion, para ejercitar dicho derecho, mediante el abono o consignacion de su importe en las referidas condiciones.\n\n"
+        "En relacion al Dominio Publico Pecuario, salvaguardando lo que pudiera resultar de los futuros deslindes, en la parcela objeto este informe, cualquier construccion, plantacion, vallado, obras, instalaciones, etc., no deberian realizarse dentro del area delimitada como Dominio Publico Pecuario provisional para evitar invadir este.\n\n"
         "En todo caso, no podrá interrumpirse el tránsito por las Vías Pecuarias, dejando siempre el paso adecuado para el tránsito ganadero y otros usos legalmente establecidos en la Ley 3/1995, de 23 de marzo, de Vías Pecuarias."
     )
     pdf.multi_cell(190, 5, texto_final, border=0, align="J")
     pdf.ln(2)
-    
-    # === CONDICIONADO:===
-    pdf.add_page()  # Nueva página
-    pdf.set_font("Arial", "B", 12)
-    pdf.cell(0, 12, "CONDICIONADO", ln=True, align="C")
-    pdf.ln(8)
-
-    # Configuración de columnas
-    margen_lateral = 15
-    ancho_total = pdf.w - 2 * margen_lateral
-    ancho_columna = (ancho_total - 5) / 2  # 5mm separación
-    line_h = 4.5
-    pdf.set_font("Arial", "", 9)
-
-    condicionado_texto = (
-        "1.- Las afecciones del presente informe se basan en cartografia oficial de la Comunidad Autonoma de la Region de Murcia y de la Direccion General del Catastro, cumpliendo el estandar tecnico Web Feature Service (WFS) definido por el Open Geospatial Consortium (OGC) y la Directiva INSPIRE, eximiendo a IBERIA FORESTAL INGENIERIA S.L de cualquier error en la cartografia.\n\n"
-        "2.- De acuerdo con lo establecido en el articulo 22.1 de la ley 43/2003 de 21 de noviembre de Montes, toda inmatriculacion o inscripcion de exceso de cabida en el Registro de la Propiedad de un monte o de una finca colindante con monte demanial o ubicado en un termino municipal en el que existan montes demaniales requerira el previo informe favorable de los titulares de dichos montes y, para los montes catalogados, el del organo forestal de la comunidad autonoma.\n\n"
-        "3.- De acuerdo con lo establecido en el articulo 25.5 de la ley 43/2003 de 21 de noviembre de Montes, para posibilitar el ejercicio del derecho de adquisicion preferente a traves de la accion de tanteo, el transmitente debera notificar fehacientemente a la Administracion publica titular de ese derecho los datos relativos al precio y caracteristicas de la transmision proyectada, la cual dispondra de un plazo de tres meses, a partir de dicha notificacion, para ejercitar dicho derecho, mediante el abono o consignacion de su importe en las referidas condiciones.\n\n"
-        "4.- En relacion al Dominio Publico Pecuario, salvaguardando lo que pudiera resultar de los futuros deslindes, en la parcela objeto este informe, cualquier construccion, plantacion, vallado, obras, instalaciones, etc., no deberian realizarse dentro del area delimitada como Dominio Publico Pecuario provisional para evitar invadir este.\n"
-        "En todo caso, no podra interrumpirse el transito por el Dominio Publico Pecuario, dejando siempre el paso adecuado para el transito ganadero y otros usos legalmente establecidos en la Ley 3/1995, de 23 de marzo, de Vias Pecuarias.\n\n"
-        "5.- El Planeamiento se regira por la Ley 13/2015, de 30 de marzo, de ordenacion territorial y urbanistica de la Region de Murcia, y por el PGOU del termino municipal. El Regimen del suelo no urbanizable se recoge en el articulo 5 de la citada Ley. Se indica que en casos de suelo no urbanizables.\n\n"
-        "6.- En suelo no urbanizable se prestara especial atencion a la Disposicion adicional segunda de la Ley 3/2020, de 27 de julio, de recuperacion y proteccion del Mar Menor, solicitando para posibles cambios de uso lo establecido en el articulo 8 de la Ley 8/2014, de 21 de noviembre, de Medidas Tributarias, de Simplificacion Administrativa y en materia de Funcion Publica.\n\n"
-        "7.- Los Planes de Gestion de la Red Natura 2000 aprobados, en la actualidad para la Comunidad Autonoma de la Region de Murcia son:\n"
-            "- Decreto n. 13/2017, de 1 de marzo - Declaracion de las ZEC \"Minas de la Celia\" y \"Cueva de las Yeseras\" y aprobacion de su Plan de Gestion.\n"
-            "- Decreto n. 259/2019, de 10 de octubre - Declaracion de ZEC y aprobacion del Plan de Gestion Integral de los Espacios Protegidos del Mar Menor y la Franja Litoral Mediterranea.\n"
-            "- Decreto n. 231/2020, de 29 de diciembre - Aprobacion del Plan de Gestion Integral de los Espacios Protegidos Red Natura 2000 de la Sierra de Ricote y La Navela.\n"
-            "- Decreto n. 47/2022, de 5 de mayo - Declaracion de ZEC y aprobacion del Plan de Gestion Integral de los Espacios Protegidos Red Natura 2000 del Alto Guadalentin; y aprobacion de los Planes de gestion de las ZEC del Cabezo de la Jara y Rambla de Nogalte y de la Sierra de Enmedio.\n"
-            "- Decreto n. 252/2022, de 22 de diciembre - Declaracion de ZEC y aprobacion del Plan de Gestion Integral de los espacios protegidos de los relieves y cuencas centro-orientales de la Region de Murcia.\n"
-            "- Decreto n. 28/2025, de 10 de abril - Declaracion de ZEC y aprobacion del Plan de Gestion Integral de los Espacios Protegidos del Altiplano de la Region de Murcia.\n\n"
-        "8.- Los Planes de Ordenacion de los Recursos Naturales aprobados, en la actualidad para la Comunidad Autonoma de la Region de Murcia son:\n"
-            "- Parque Regional Sierra de la Pila - Decreto n 43/2004, de 14 de mayo (aprobado definitivamente; BORM n 130, de 07/06/2004).\n"
-            "- Parque Regional Sierra de El Carche - Decreto n 69/2002, de 22 de marzo (aprobado; BORM n 77, de 04/04/2002).\n"
-            "- Parque Regional Salinas y Arenales de San Pedro del Pinatar - Decreto 44/1995, de 26 de mayo de 1995 (BORM n 151, de 01/07/1995).\n"
-            "- Parque Regional Calblanque, Monte de las Cenizas y Pena del Aguila - Decreto 45/1995, de 26 de mayo de 1995 (BORM n 152, de 03/07/1995).\n"
-            "- Parque Regional Sierra Espuna (incluido el Paisaje Protegido Barrancos de Gebas) - Decreto 13/1995, de 31 de marzo de 1995 (aprobacion del PORN; BORM n 85, de 11/04/1995).\n"
-            "- Humedal del Ajauque y Rambla Salada - Orden (1998) (fase inicial).\n"
-            "- Saladares del Guadalentin - Orden (29/12/1998) (fase inicial).\n"
-            "- Sierra de Salinas - Orden (03/07/2002) (fase inicial).\n"
-            "- Carrascoy y El Valle - Orden (18/05/2005) (fase inicial - ademas, existe en 2025 proyecto de Plan / Plan de Gestion/ZEC en informacion publica).\n"
-            "- Sierra de la Muela, Cabo Tinoso y Roldan - Orden (15/03/2006) (fase inicial).\n\n"
-        "9.- Los Planes de Recuperacion de Flora aprobados, en la actualidad para la Comunidad Autonoma de la Region de Murcia son:\n"
-            "- Decreto 244/2014, de 19 de diciembre: aprueba los planes de recuperacion de las especies Cistus heterophyllus subsp. carthaginensis, Erica arborea, Juniperus turbinata, Narcissus nevadensis subsp. enemeritoi y Scrophularia arguta. Publicado en BORM n 297, de 27/12/2014.\n"
-            "- Decreto 12/2007, de 22 de febrero: aprueba el plan de recuperacion de la especie Astragalus nitidiflorus (\"garbancillo de Tallante\"). Publicado en BORM n 51, de 3/03/2007.\n\n"
-        "10.- Los Planes de Recuperacion de Fauna aprobados, en la actualidad para la Comunidad Autonoma de la Region de Murcia son:\n"
-            "- Decreto n. 59/2016, de 22 de junio, de aprobacion de los planes de recuperacion del aguila perdicera, la nutria y el fartet.\n"
-            "- Decreto n. 70/2016, de 12 de julio - Catalogacion de la malvasia cabeciblanca como especie en peligro de extincion y aprobacion de su Plan de Recuperacion en la Region de Murcia."
-    )
-    
-    # --- TODAS LAS LÍNEAS EN UNA LISTA ---
-    parrafos = [p.strip() for p in condicionado_texto.split('\n\n') if p.strip()]
-
-    # --- DIVIDIR LÍNEAS EN 2 GRUPOS DE ALTURA SIMILAR ---
-    col1_parrafos = []
-    col2_parrafos = []
-    altura_col1 = 0
-    altura_col2 = 0
-
-    for parrafo in parrafos:
-        # Estimar altura
-        h_parrafo = 0
-        for linea in parrafo.split('\n'):
-            if linea.strip():
-                line_width = pdf.get_string_width(linea)
-                num_lineas = max(1, int(line_width / ancho_columna) + 1)
-                h_parrafo += num_lineas * line_h
-            else:
-                h_parrafo += line_h
-
-        if altura_col1 <= altura_col2:
-            col1_parrafos.append(parrafo)
-            altura_col1 += h_parrafo
-        else:
-            col2_parrafos.append(parrafo)
-            altura_col2 += h_parrafo
-
-    # --- GUARDAR POSICIÓN INICIAL ---
-    y_inicio = pdf.get_y()
-    pdf.set_x(margen_lateral)
-
-    # --- ESCRIBIR COLUMNA 1 ---
-    for parrafo in col1_parrafos:
-        pdf.multi_cell(ancho_columna, line_h, parrafo, align="J")
-       
-    y_final_col1 = pdf.get_y()
-
-    # --- ESCRIBIR COLUMNA 2 (misma altura que la 1) ---
-    pdf.set_xy(margen_lateral + ancho_columna + 5, y_inicio)
-    for parrafo in col2_parrafos:
-        pdf.multi_cell(ancho_columna, line_h, parrafo, align="J")
-        
-    # Ajustar altura final
-    pdf.set_y(max(y_final_col1, pdf.get_y()))   
         
     # === PIE ===
     pdf.ln(10)
     pdf.set_font("Arial", "", 9)
-    pdf.multi_cell(0, line_h,
-        "La normativa de referencia esta actualizada a fecha de uno de enero de dos mil veintiseis, y sera revisada trimestralmente.\n\n"
+    pdf.multi_cell(0, line_h,        
         "Para mas informacion:\n"
         "E-mail: info@iberiaforestal.es",
         align="J"
