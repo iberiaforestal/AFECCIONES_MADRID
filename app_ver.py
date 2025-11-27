6414a560fa85b321d2939e65390cbdc3fcbb2939
iberiaforestal <iberiaforestal.ing@gmail.com>
2025-11-27 17:55:40 +0100

    Delete app_ver.py

--- app_ver.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-st.title("TODO OK")
-st.write("https://informes.iberiaforestal.es")
-st.write("HTTPS activo")
-st.write("Dominio funcionando")
-st.write("Listo para tus 17 herramientas")
