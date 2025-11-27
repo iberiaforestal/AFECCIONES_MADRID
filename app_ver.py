e4e2b093085a5575de090a744324c7f18ffd6d85
iberiaforestal <iberiaforestal.ing@gmail.com>
2025-11-27 15:05:01 +0100

    Add files via upload

---
+++ app_ver.py
@@ -0,0 +1,6 @@
+import streamlit as st
+st.title("TODO OK")
+st.write("https://informes.iberiaforestal.es")
+st.write("HTTPS activo")
+st.write("Dominio funcionando")
+st.write("Listo para tus 17 herramientas")
