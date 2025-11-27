20d17c7ba3f414b6e532ba8b6e7754abcd10d82e
iberiaforestal <iberiaforestal.ing@gmail.com>
2025-11-27 13:17:59 +0100

    Update madrid_VPS.py

--- madrid_VPS.py
+++ madrid_VPS.py
@@ -1636,10 +1636,8 @@ def generar_pdf(datos, x, y, filename):
     return filename
 
 # Interfaz de Streamlit
-st.image(
-    "/home/ubuntu/informes/comunidades/Madrid/logos.jpg",
-    width=250 # ← más pequeño (prueba 160-200)
-)
+st.image(Image.open(logo_path), width=250)
+
 st.title("Informe basico de Afecciones al Medio Natural")
 
 modo = st.radio("Seleccione el modo de búsqueda. Recuerde que la busqueda por parcela analiza afecciones al total de la superficie de la parcela, por el contrario la busqueda por coodenadas analiza las afecciones del punto", ["Por coordenadas", "Por parcela"])
